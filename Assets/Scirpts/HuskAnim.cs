7f59214a6f08eb53fa5fefd86209333e53d075b8
Reedbetweenthelines <marcusreed1324@gmail.com>
2021-05-06 16:59:29 -0400

    Base

---
+++ Assets/Scirpts/HuskAnim.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HuskAnim : MonoBehaviour
+{
+
+    Animator husker;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        husker = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            husker.SetInteger("Form", 1);
+            
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                husker.SetInteger("Form", 2);
+
+            }
+        }
+        else
+        {         
+            husker.SetInteger("Form", 0);            
+        }
+
+
+    }
+}
